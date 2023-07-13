4f11ab1c7ac6bd0d2a4da88b99fefff7fee13c51
praveenbhat73 <bhatpraveen721@gmail.com>
2023-07-13 09:46:15 +0530

    code

---
+++ src/components/Background.jsx
@@ -0,0 +1,22 @@
+
+const Background = () => {
+    return (
+      <ul className="background">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    )
+  }
+  
+  export default Background
